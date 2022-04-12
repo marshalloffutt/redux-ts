f4b725114a969b0a3b0a69cf0ad562568d37eb46
Marshall Offutt <marshalloffutt@gmail.com>
2022-04-12 18:41:00 -0500

    Reorganize reducer logic, adds action creator.

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { ActionTypes } from '../action-types';
+import { Action } from '../actions';
+
+export const searchRepositories = (term: string) => {
+	return async (dispatch: any) => {
+		dispatch({
+			type: ActionTypes.SEARCH_REPOSITORIES
+		});
+
+		try {
+			const { data } = await axios.get(
+				'https://registry.npmjs.org/-/v1/search',
+				{
+					params: {
+						text: term
+					},
+				});
+
+			const names = data.objects.map((result: any) => {
+				return result.package.name;
+			});
+
+			dispatch({
+				type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+				payload: names
+			})
+		} catch (err: any) {
+			dispatch({
+				type: ActionTypes.SEARCH_REPOSITORIES_ERROR,
+				payload: err.message
+			});
+		}
+	};
+};
--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,46 +1,27 @@
+import { ActionTypes } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
 	loading: boolean;
 	error: string | null;
 	data: string[];
 }
 
-interface SearchRepositoriesAction {
-	type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-	type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-	payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-	type: ActionType.SEARCH_REPOSITORIES_ERROR;
-	payload: string;
-}
-
-enum ActionType {
-	SEARCH_REPOSITORIES = 'search_repositories',
-	SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-	SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction;
-
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
 	switch (action.type) {
-		case ActionType.SEARCH_REPOSITORIES:
+		case ActionTypes.SEARCH_REPOSITORIES:
 			return {
 				loading: true,
 				error: null,
 				data: []
 			}
-		case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+		case ActionTypes.SEARCH_REPOSITORIES_SUCCESS:
 			return {
 				loading: false,
 				error: null,
 				data: action.payload
 			}
-		case ActionType.SEARCH_REPOSITORIES_ERROR:
+		case ActionTypes.SEARCH_REPOSITORIES_ERROR:
 			return {
 				loading: false,
 				error: action.payload,
